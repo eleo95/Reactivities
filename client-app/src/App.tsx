2aa5ffb2d2ed9a51a082b2ac02c44f747f1ea117
eleo95 <e.leonardo7474@gmail.com>
2023-06-01 21:25:31 -0400

    End S3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Header, List } from 'semantic-ui-react'
+
+function App() {
+  const [activities,setActivities] = useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/api/activities')
+    .then(response => {
+      // console.log("resp",response)
+      setActivities(response.data)
+    })
+  },[])
+
+  return (
+    <>
+      <Header as="h2" icon="users" content="Reactivities" />
+      <List>
+        {
+          activities.map((activity:any)=>{
+            return (
+              <List.Item key={activity.id}>{activity.title}</List.Item>
+            )
+          })
+        }
+      </List>
+    </>
+  )
+}
+
+export default App
